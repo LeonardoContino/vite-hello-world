edefb78df5b9946b15a3cdb9e19ca3c85d01ab26
LeonardoContino <leoconti2000@hotmail.it>
2023-01-19 15:43:29 +0100

    aggiunto titolo

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <script>
+import AppTitle from './components/AppTitle.vue';
 
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  components:{
+    AppTitle
+  }
+}
 </script>
 
 <template>
-  
+  <AppTitle></AppTitle>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data(){
+        name: 'AppTitle'
+        return{
+            title: 'primo sito con vite'
+        }
+    }
+}
+</script>
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<style>
+ h1{
+    text-align: center;
+ }
+</style>
